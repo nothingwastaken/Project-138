3bc139d8a6d8713080addd92be43a7aa17220b41
nothingwastaken <82754118+nothingwastaken@users.noreply.github.com>
2023-06-15 18:19:26 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -23,11 +23,23 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+  canvas.center();
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
 }
 
+function modelLoaded(){
+  console.log("ready");
+}
 
 function draw(){
 
+ image(video, 0, 0, 700, 600);
  background(0); 
 
  fill("black");
